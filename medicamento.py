ca2432f7731b381b6b7ab2e6a91c04d905e27e83
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-03-06 17:57:09 -0300

    Segundo commit, completando comentarios y agregando print de atributos

--- medicamento.py
+++ medicamento.py
@@ -1,8 +1,9 @@
 
-# archivo medicamento.py
+# Clase Medicamento
 class Medicamento():
     IVA = 0.18
 
+    # Método constructor y sus parámteros con valores por defecto
     def __init__(self, nombre: str, stock: int = 0):
         self.nombre = nombre
         self.stock = stock
@@ -10,32 +11,38 @@ class Medicamento():
         self.precio_final = 0.0
         self.descuento = 0.0
 
+    # Método estático que verifica si valor ingresado es mayor a cero
     @staticmethod
     def validar_mayor_a_cero(numero: int):
         return numero > 0
     
+    # Método getter que retorna el precio final del objeto
     @property
     def precio(self):
         return self.precio_final
     
+    # Método setter que establece el precio final y el descuento en base al precio bruto
     @precio.setter
     def precio(self, precio_bruto: int):
         if self.validar_mayor_a_cero(precio_bruto):
             self.precio_bruto = precio_bruto
             self.precio_final = precio_bruto + precio_bruto * self.IVA
 
+            # Establece valor de descuento en caso de que corresponda
             if self.precio_final >= 10000 and self.precio_final < 20000:
                 self.descuento = 0.1
             elif self.precio_final >= 20000:
                 self.descuento = 0.2
         
+            # Si existe descuento se aplica
             if self.descuento:
                 self.precio_final *= 1 - self.descuento
 
-    # Este es el método que se debe agregar
+    # Método de igualdad que comprueba si dos medicamentos son iguales basándose en sus nombres
     def __eq__(self, other):
         return self.nombre.lower() == other.nombre.lower()
 
+    # Método para sumar el stock de dos medicamentos si son iguales
     def __iadd__(self, other):
         if self == other:
             self.stock += other.stock
--- programa.py
+++ programa.py
@@ -1,33 +1,38 @@
 
-# archivo programa.py
 from medicamento import Medicamento
 
 opcion_ingreso = int(input("¿Desea agregar un medicamento?"
 "\n1. Sí\n2. No\n"))
+
+# Lista que almacena los objetos medicamento
 ingresados = []
 
+# Ciclo del cual solo se pueda salir cuando la opción sea diferente de 1
 while opcion_ingreso == 1:
     nombre = input("\nIngrese nombre del medicamento:\n")
     stock = int(input("\nIngrese stock del medicamento:\n"))
     m = Medicamento(nombre, stock)
 
+    # Si medicamento existe en la lista 
     if m in ingresados:
+
+        # Sumar el stock al medicamento existente
         indice = ingresados.index(m)
         ingresados[indice] += m
 
-        # Corrección José Contreras.
+        # Código modificado. Llama a los atributos del objeto ya existente
         nombre = ingresados[indice].nombre
         precio_bruto = ingresados[indice].precio_bruto
         precio_final = ingresados[indice].precio_final
         stock = ingresados[indice].stock
 
+        # Imprime atributos
         print(f"\n***** DATOS MEDICAMENTO {nombre} *****")
         print(f"PRECIO BRUTO: ${precio_bruto}")
-
         print(f"PRECIO FINAL: ${precio_final}")
         print(f"STOCK: {stock}")
         
-
+    # Si no existe lo agrega a la lista e imprime sus atributos
     else:
         ingresados.append(m)
         precio_bruto = int(input("\nIngrese precio bruto del medicamento:\n"))
@@ -36,13 +41,32 @@ while opcion_ingreso == 1:
         print(f"\n***** DATOS MEDICAMENTO {m.nombre} *****")
         print(f"PRECIO BRUTO: ${m.precio_bruto}")
 
+        # Si tiene descuento lo imprime
         if m.descuento:
             print(f"DESCUENTO: {m.descuento*100}%")
 
         print(f"PRECIO FINAL: ${m.precio_final}")
         print(f"STOCK: {m.stock}")
         
-
+    # Imprime el número de medicamentos de la lista
     print(f"\nLa farmacia cuenta con {len(ingresados)} medicamento(s)\n")
     opcion_ingreso = int(input("¿Desea agregar un medicamento?"
                                "\n1. Sí\n2. No\n"))
+    
+# Imprimir lista de medicamentos (objetos)
+    print(ingresados)
+
+# Imprime lista de medicamentos (objetos)
+for index, ing in enumerate(ingresados):
+    print(ing)
+
+# Imprime atributos de cada objetos
+for index, ing in enumerate(ingresados):
+    print()
+    print(f"Medicamento {index + 1}:")
+    print(f"Nombre: {ing.nombre}")
+    print(f"Stock: {ing.stock}")
+    print(f"Precio bruto: {ing.precio_bruto}")
+    print(f"Precio final: {ing.precio_final}")
+    print(f"Descuento: {ing.descuento}")
+    
